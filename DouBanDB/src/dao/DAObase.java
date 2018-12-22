a7fa417bdfd1036007f9323c404cfe07bfbb3f7a
sunlilong <1255585189@qq.com>
2018-12-22 16:21:31 +0800

    finish DAObase.java

---
+++ DouBanDB/src/dao/DAObase.java
@@ -0,0 +1,24 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DAObase implements DAO {
+
+	@Override
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://115.28.165.3:3306/Dva";
+			conn = DriverManager.getConnection(url,"dva","123456");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+}
